7768def0f39684280b47c8f87663313944433988
李铭昕 <715557344@qq.com>
2020-07-24 16:21:02 +0800

    Update

---
+++ config/autoload/tencent.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+return [
+    'app_id' => env('APP_ID'),
+    'app_key' => env('APP_KEY'),
+];
