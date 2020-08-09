49de9b37119b11a16a80e0901dc8eb45d7d08644
李铭昕 <715557344@qq.com>
2020-08-09 18:13:27 +0800

    Update

--- app/Service/WeChat.php
+++ app/Service/WeChat.php
@@ -14,10 +14,17 @@ namespace App\Service;
 use App\Service\Handler\HandlerInterface;
 use EasyWeChat\Factory;
 use EasyWeChat\OfficialAccount\Application;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
 use Symfony\Component\HttpFoundation\Request;
 
 class WeChat extends Service
 {
+    /**
+     * @var HandlerStack
+     */
+    protected $stack;
+
     public function getApp(Request $request): Application
     {
         $config = array_merge($this->config->get('wechat', []), [
@@ -26,9 +33,22 @@ class WeChat extends Service
 
         $app = Factory::officialAccount($config);
 
+        // 重写 Request
+        $app['request'] = $request;
+
+        // 重写 Guzzle Client
+        $config = $app['config']->get('http', []);
+        $config['handler'] = $this->getStack();
+        $app->rebind('http_client', new Client($config));
+
         // 重写 Handler
         $app['guzzle_handler'] = $this->handler;
-        $app['request'] = $request;
+
+        // 重写 Socialite
+        $app->oauth->setGuzzleOptions([
+            'http_errors' => false,
+            'handler' => $this->getStack(),
+        ]);
 
         $app->server->push(function ($message) {
             $this->logger->info((string) json_encode($message, JSON_UNESCAPED_UNICODE));
@@ -47,5 +67,14 @@ class WeChat extends Service
         });
 
         return $app;
+    }
+
+    protected function getStack()
+    {
+        if ($this->stack instanceof HandlerStack) {
+            return $this->stack;
+        }
+
+        return $this->stack = HandlerStack::create($this->handler);
     }
 }
